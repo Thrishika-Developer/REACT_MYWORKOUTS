a3e63d04c856124fe73be2098979f9ac05269578
Thrishika-Developer <thrishikasinger999@gmail.com>
2026-05-05 15:28:53 +0530

    added

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import NameList from './pages/a_events/NameList';
+import NameList from './pages/a_events/NameList';
 // import InputEvents from './pages/a_events/InputEvents';
-import StateEvents from './pages/tasks/StateEvents';
-import Dropdown from './pages/tasks/Dropdown';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
       {/* <InputEvents/> */}
-      {/* <NameList/> */}
-      {/* <StateEvents/> */}
-      <Dropdown/>
+      <NameList/>
+
   </React.StrictMode>
 );
 
--- src/pages/a_events/NameList.jsx
+++ src/pages/a_events/NameList.jsx
@@ -1,21 +1,22 @@
-// import { useState } from "react";
-// export default function NameList(){
-//     const [input,setInput]=useState()
-//     const [nameList,setNameList]=useState([])
+import { useState } from "react";
+export default function NameList(){
+    const [input,setInput]=useState()
+    const [nameList,setNameList]=useState([])
 
-//     return <div>
+    return <div>
 
-//         <div>
-//             <h5>Add Name List</h5>
-//             <input type="text" onChange={(events)=>setInput(events.target.value)}/>
-//             <button onClick={()=>setNameList([...nameList,input])}>Add</button>
-//         </div>
+        <div>
+            <h5>Add Name List</h5>
+            <input type="text" onChange={(events)=>setInput(events.target.value)}/>
+            <button onClick={()=>setNameList([...nameList,input])}>Add</button>            
+        </div>
 
-//         <div>
-//             <h5>View Name List</h5>
-//             <div>
-//                 {nameList.map((value,index)=><li key={index}>{value}</li>)}
-//             </div>
-//         </div>
-//     </div>
-// }
+        <div>
+            <h5>View Name List</h5>
+            <div>
+                {nameList.map((value,index)=><li key={index}>{value}</li>)}
+              
+            </div>
+        </div>
+    </div>
+}
