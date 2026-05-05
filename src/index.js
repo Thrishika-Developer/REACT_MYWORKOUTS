c95bfda5fdd0b54288058045e5338594c1923881
Thrishika-Developer <thrishikasinger999@gmail.com>
2026-05-05 06:44:19 +0530

    added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import DemoPage2 from './pages/DemoPage2';
-import DemoPage1 from './pages/DemoPage1';
-import DemoPage3 from './pages/DemoPage3';
-import DemoPage4 from './pages/DemoPage4';
-import DemoPage5 from './pages/DemoPage5';
+// import DemoPage2 from './pages/DemoPage2';
+// import DemoPage1 from './pages/DemoPage1';
+// import DemoPage3 from './pages/DemoPage3';
+// import DemoPage4 from './pages/DemoPage4';
+// import DemoPage5 from './pages/DemoPage5';
+import Counter from './pages/demoPage6/props_callback/Counter';
+import DemoPage7 from './pages/demopage7/Proper_web/DemoPage7';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -14,7 +16,9 @@ root.render(
    {/* <DemoPage2/> */}
    {/* <DemoPage3/> */}
    {/* <DemoPage4/> */}
-   <DemoPage5/>
+   {/* <DemoPage5/> */}
+   {/* <Counter/> */}
+   <DemoPage7/>
   </React.StrictMode>
 );
 
---
+++ src/pages/demoPage6/props_callback/Button.jsx
@@ -0,0 +1,12 @@
+export default function Button( 
+    {count,addcount,subcount,reset,squareroot})
+    {
+
+    return <div>
+        <button onClick={()=>addcount(1)}>Add</button>
+        <button onClick={()=>subcount(1)}>sub</button>
+        <button onClick={()=>squareroot(2)}>squareroot</button>
+        <button onClick={()=>reset(0)}>reset</button>
+    </div>
+}
+
